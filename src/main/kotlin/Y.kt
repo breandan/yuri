ab530bdd23b353ef0527f63ca67e4116155efa44
Breandan Considine <breandan.considine@nutonomy.com>
2018-07-29 14:39:57 -0400

    refactor and cleanup

--- src/main/kotlin/Y.kt
+++ src/main/kotlin/Y.kt
@@ -6,67 +6,58 @@ abstract class Directory<T> {}
 abstract class File<T> {}
 
 open class Y<A> private constructor() {
-  open class localhost_: Directory<Nothing>() {
+  open class localhost_ {
     @JvmName("usr")
-    operator fun div(a: usr<localhost_>.() -> usr.Companion) = usr
+    operator fun div(a: usr.Companion) = usr<localhost_>()
     @JvmName("bin")
-    operator fun div(a: bin<localhost_>.() -> bin.Companion) = bin
+    operator fun div(a: bin.Companion) = bin<localhost_>()
     @JvmName("etc")
-    operator fun div(a: etc<localhost_>.() -> etc.Companion) = etc
+    operator fun div(a: etc.Companion) = etc<localhost_>()
 
-    companion object: localhost_() {}
+    companion object: localhost_()
   }
 
-  open class bin<T>: Directory<T>() {
-    companion object: (bin<*>) -> Companion {
-      override fun invoke(a: bin<*>) = this
-      @JvmName("localhost_")
-      operator fun div(a: sh<bin<localhost_>>.() -> sh.Companion) = sh
-      @JvmName("usr_localhost_")
-      operator fun div(a: vim<bin<usr<localhost_>>>.() -> vim.Companion) = vim
-    }
+  open class bin<T>: Directory<T>(), (bin<T>) -> bin<T> {
+    override fun invoke(p1: bin<T>): bin<T> = this
+    companion object
   }
 
-  class sh<T>: File<T>() {
-    companion object: (sh<bin<localhost_>>) -> Companion {
-      override fun invoke(a: sh<bin<localhost_>>) = this
-    }
+  class sh<T>: File<T>(), (sh<T>) -> sh<T> {
+    override fun invoke(p1: sh<T>): sh<T> = this
+    companion object
   }
 
-  open class etc<T>: Directory<T>() {
-    companion object: (etc<*>) -> Companion {
-      override fun invoke(a: etc<*>) = this
-      operator fun div(a: vim<etc<localhost_>>.() -> vim.Companion) = vim
-    }
+  open class etc<T>: Directory<T>(), (etc<T>) -> etc<T> {
+    override fun invoke(p1: etc<T>): etc<T> = this
+    companion object
   }
 
-  class vim<T>: File<T>() {
-    companion object: (vim<*>) -> Companion {
-      override fun invoke(a: vim<*>) = this
-    }
+  class vim<T>: File<T>(), (vim<T>) -> vim<T> {
+    override fun invoke(p1: vim<T>): vim<T> = this
+    companion object
   }
 
-  open class usr<T>: Directory<T>() {
-    companion object: (usr<*>) -> Companion {
-      override fun invoke(a: usr<*>) = this
-      operator fun div(a: local<usr<localhost_>>.() -> local.Companion) = local
-      operator fun div(a: bin<usr<localhost_>>.() -> bin.Companion) = bin
-    }
+  open class usr<T>: Directory<T>(), (usr<T>) -> usr<T> {
+    override fun invoke(p1: usr<T>): usr<T> = this
+    companion object
   }
 
-  open class local<T>: Directory<T>() {
-    companion object: (local<*>) -> Companion {
-      override fun invoke(a: local<*>) = this
-      operator fun div(a: bin<local<usr<localhost_>>>.() -> bin.Companion) = bin
-    }
+  open class local<T>: Directory<T>(), (local<T>) -> local<T> {
+    override fun invoke(p1: local<T>): local<T> = this
+    companion object
   }
 
   companion object {
-    fun uri(path: Any) {
-      TODO()
-    }
+    fun uri(path: Any) = println()
   }
 }
+
+operator fun bin<localhost_>.div(a: sh.Companion) = sh<bin<localhost_>>()
+operator fun bin<usr<localhost_>>.div(a: vim.Companion) = vim<bin<usr<localhost_>>>()
+operator fun etc<localhost_>.div(a: vim.Companion) = vim<etc<localhost_>>()
+operator fun usr<localhost_>.div(a: bin.Companion) = bin<usr<localhost_>>()
+operator fun usr<localhost_>.div(a: local.Companion) = local<usr<localhost_>>()
+operator fun local<usr<localhost_>>.div(a: bin.Companion) = bin<local<usr<localhost_>>>()
 
 /**
  * localhost_/
@@ -84,9 +75,7 @@ open class Y<A> private constructor() {
 fun main(a: Array<String>) {
   //Compiles!
   Y.uri(localhost_)
-  Y.uri(localhost_/bin)
   Y.uri(localhost_/bin/sh)
-  Y.uri(localhost_/etc)
   Y.uri(localhost_/etc/vim)
   Y.uri(localhost_/usr)
   Y.uri(localhost_/usr/bin/vim)
@@ -95,6 +84,7 @@ fun main(a: Array<String>) {
 
   //Does not compile!
   Y.uri(localhost_/local)
+  Y.uri(localhost_/bin/vim)
   Y.uri(localhost_/sh)
   Y.uri(localhost_/bin/local)
   Y.uri(localhost_/etc/local)
