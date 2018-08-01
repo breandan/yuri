e200a285776b1cdb157d915e1151f6f9caee97ae
Breandan Considine <breandan.considine@nutonomy.com>
2018-08-01 19:11:26 -0400

    provide simple non-working Java completion example

--- src/main/java/J.java
+++ src/main/java/J.java
@@ -1,34 +1,23 @@
-//import main.kotlin.Directory;
-
 public class J  {
-//    public static class localhost<T> extends Directory<T> {
-//        public static root<localhost> div(root<localhost> a) {
-//            return a;
-//        }
-//    }
-//
-//    public static class root<T> extends Directory<T> {
-//        public static Directory<root> div(Directory<root> a) {
-//            return a;
-//        }
-//    }
-//
-//    public static class bin<T> extends Directory<T> {}
-//
-//    private J() {};
-//
-//    public static final J uri = new J();
-//    public static final localhost localhost = new localhost();
-//
-//    public static final root<localhost> root = new root<localhost>();
-//    public static final bin<root> bin = new bin<root>();
-//
-//
-//    public localhost div(localhost localhost) {
-//        return localhost;
-//    }
-//
-//    static {
-//        localhost.div(root).div(bin);
-//    }
+    static class localhost {
+        static root div(root a) {
+            return a;
+        }
+    }
+
+    public static class root  {
+        static bin div(bin a) {
+            return a;
+        }
+    }
+
+    static class bin {}
+
+    static final localhost localhost = new localhost();
+    static final root root = new root();
+    static final bin bin = new bin();
+
+    public static void main(String[] args) {
+        localhost.div(root); // Try to complete: localhost.div(<caret>)
+    }
 }
