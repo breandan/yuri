67df56fe79f5c6e0a550d7180bf5a62d09f5b384
Breandan Considine <breandan.considine@nutonomy.com>
2018-07-31 21:21:09 -0400

    simplify with type bound

--- src/main/kotlin/Y.kt
+++ src/main/kotlin/Y.kt
@@ -5,13 +5,8 @@ import main.kotlin.Y.*
 @DslMarker
 annotation class Yuri
 
-abstract class File<T>( path: String ) {
-  open val path: String = "$path/${javaClass.simpleName}"
-  override fun toString() = path
-}
-
 @Yuri
-open class Y private constructor() {
+abstract class Y private constructor(path: String) {
   @Yuri open class localhost_ {
     @JvmName("usr") operator fun div(a: usr.Companion) = usr<localhost_>("/")
     @JvmName("bin") operator fun div(a: bin.Companion) = bin<localhost_>("/")
@@ -20,13 +15,16 @@ open class Y private constructor() {
     @Yuri companion object: localhost_()
   }
 
-  @Yuri open class bin<T>(path: String): File<T>(path) { @Yuri companion object }
-  @Yuri open class sh<T>(path: String): File<T>(path) { @Yuri companion object }
-  @Yuri open class etc<T>(path: String) : File<T>(path) { @Yuri companion object }
-  @Yuri open class vim<T>(path: String): File<T>(path) { @Yuri companion object }
-  @Yuri open class usr<T>(path: String): File<T>(path) { @Yuri companion object }
-  @Yuri open class local<T>(path: String): File<T>(path) { @Yuri companion object }
+  @Yuri open class bin<T>(path: String): Y(path) { @Yuri companion object }
+  @Yuri open class sh<T>(path: String): Y(path) { @Yuri companion object }
+  @Yuri open class etc<T>(path: String) : Y(path) { @Yuri companion object }
+  @Yuri open class vim<T>(path: String): Y(path) { @Yuri companion object }
+  @Yuri open class usr<T>(path: String): Y(path) { @Yuri companion object }
+  @Yuri open class local<T>(path: String): Y(path) { @Yuri companion object }
   @Yuri companion object { @Yuri fun uri(path: Any) = println(path) }
+
+  internal open val path: String = "$path/${javaClass.simpleName}"
+  override fun toString() = path
 }
 
 /**
@@ -42,9 +40,9 @@ open class Y private constructor() {
  *         └── bin/
  */
 
-@Yuri operator fun bin<localhost_>.div(a: sh.Companion) = sh<bin<localhost_>>(path)
-@Yuri operator fun bin<usr<localhost_>>.div(a: vim.Companion) = vim<bin<usr<localhost_>>>(path)
-@Yuri operator fun etc<localhost_>.div(a: vim.Companion) = vim<etc<localhost_>>(path)
-@Yuri operator fun usr<localhost_>.div(a: bin.Companion) = bin<usr<localhost_>>(path)
-@Yuri operator fun usr<localhost_>.div(a: local.Companion) = local<usr<localhost_>>(path)
-@Yuri operator fun local<usr<localhost_>>.div(a: bin.Companion) = bin<local<usr<localhost_>>>(path)
+@Yuri operator fun <S: bin<localhost_>> S.div(a: sh.Companion) = sh<S>(path)
+@Yuri operator fun <S: bin<usr<localhost_>>> S.div(a: vim.Companion) = vim<S>(path)
+@Yuri operator fun <S: etc<localhost_>> S.div(a: vim.Companion) = vim<S>(path)
+@Yuri operator fun <S: usr<localhost_>> S.div(a: bin.Companion) = bin<S>(path)
+@Yuri operator fun <S: usr<localhost_>> S.div(a: local.Companion) = local<S>(path)
+@Yuri operator fun <S: local<usr<localhost_>>> S.div(a: bin.Companion) = bin<S>(path)
