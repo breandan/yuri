05aeb7b76960ffbc9e602524445e35f5811dae81
Breandan Considine <breandan.considine@nutonomy.com>
2018-08-05 16:47:10 -0400

    implement kleene-star syntax

--- src/main/kotlin/HelloYuri.kt
+++ src/main/kotlin/HelloYuri.kt
@@ -3,24 +3,27 @@ package main.kotlin
 import main.kotlin.Y.*
 import main.kotlin.G.*
 
-/**
- * localhost/
- * ├── bin/
- * │   └── sh
- * ├── etc/
- * │   └── vim
- * └── usr/
- *     ├── bin/
- *     │   └── vim
- *     └── local/
- *         └── bin/
- */
-
 fun main(a: Array<String>) {
-  //Compiles!
+  println(""" MOCK FILE TREE:
+
+  localhost
+  ├── bin
+  │   └── sh
+  ├── etc
+  │   └── vim
+  └── usr
+      ├── bin
+      │   └── vim
+      └── local
+          └── bin
+              └── sh
+  """)
+
+  println("YURI-VALIDATED URIs:")
+
   listOf(
+      // Compiles!
       Y.uri(localhost),
-      Y.uri(localhost /bin),
       Y.uri(localhost /bin),
       Y.uri(localhost /bin/sh),
       Y.uri(localhost /etc),
@@ -29,24 +32,41 @@ fun main(a: Array<String>) {
       Y.uri(localhost /usr/bin/vim),
       Y.uri(localhost /usr/local),
       Y.uri(localhost /usr/local/bin),
-
-      //Does not compile!
-//      Y.uri(localhost /local),
-//      Y.uri(localhost /bin/vim),
-//      Y.uri(localhost /sh),
-//      Y.uri(localhost /bin/local),
-//      Y.uri(localhost /etc/local),
-//      Y.uri(localhost /etc/sh),
-//      Y.uri(localhost /usr/local/sh),
-//      G.uri(project /test),
-//      G.uri(project /compileKotlin),
-//      G.uri(project /kotlin),
-//      G.uri(project /production/classes/main),
-
+      Y.uri(localhost /usr/local/bin/sh),
       G.uri(project /gradle),
       G.uri(project /gradlew),
       G.uri(project /settings_dot_gradle),
       G.uri(project /src/main),
       G.uri(project /src/main/kotlin)
+
+      // Does not compile!
+      // ,Y.uri(localhost /local),
+      // ,Y.uri(localhost /bin/vim),
+      // ,Y.uri(localhost /sh),
+      // ,Y.uri(localhost /bin/local),
+      // ,Y.uri(localhost /etc/local),
+      // ,Y.uri(localhost /etc/sh),
+      // ,Y.uri(localhost /usr/local/sh),
+      // ,G.uri(project /test),
+      // ,G.uri(project /compileKotlin),
+      // ,G.uri(project /kotlin),
+      // ,G.uri(project /production/classes/main)
   ).forEach { println("$it") }
+
+  println("\nKLEENE STAR SEARCH:\n")
+
+  listOf(
+      // Compiles!
+      localhost *bin,
+      localhost *sh,
+      localhost *vim,
+      localhost *bin/sh,
+      localhost *etc/vim,
+      localhost *usr/bin
+
+      // Does not compile!
+      // ,localhost *etc/bin
+      // ,localhost *etc/sh
+      // ,localhost *local/usr
+  ).forEach { println("*$it -> ${Y.uris(it)}") }
 }
--- src/main/kotlin/Y.kt
+++ src/main/kotlin/Y.kt
@@ -9,41 +9,80 @@ annotation class Yuri
 private const val unused = "UNUSED_PARAMETER"
 
 @Yuri
-abstract class Y private constructor(val uri: String = "/") {
-  @Yuri object localhost: Y("/") {
-    @JvmName("usr") operator fun div(@Suppress(unused) a:usr.Companion) = usr<localhost>(uri)
-    @JvmName("bin") operator fun div(@Suppress(unused) a:bin.Companion) = bin<localhost>(uri)
-    @JvmName("etc") operator fun div(@Suppress(unused) a:etc.Companion) = etc<localhost>(uri)
+abstract class Y private constructor(open val uri: String = "/") {
+  @Yuri object localhost: Y("") {
+    @JvmName("root_usr") operator fun div(@Suppress(unused) a:usr.Companion) = usr<localhost>(uri)
+    @JvmName("root_bin") operator fun div(@Suppress(unused) a:bin.Companion) = bin<localhost>(uri)
+    @JvmName("root_etc") operator fun div(@Suppress(unused) a:etc.Companion) = etc<localhost>(uri)
+
+    @JvmName("any_usr") operator fun times(@Suppress(unused) a:usr.Companion) = usr<Y>(uri)
+    @JvmName("any_bin") operator fun times(@Suppress(unused) a:bin.Companion) = bin<Y>(uri)
+    @JvmName("any_etc") operator fun times(@Suppress(unused) a:etc.Companion) = etc<Y>(uri)
+    @JvmName("any_local") operator fun times(@Suppress(unused) a:local.Companion) = local<Y>(uri)
+    @JvmName("any_sh") operator fun times(@Suppress(unused) a:sh.Companion) = sh<Y>(uri)
+    @JvmName("any_vim") operator fun times(@Suppress(unused) a:vim.Companion) = vim<Y>(uri)
+
     override fun toString() = uri
   }
 
-  @Yuri open class bin<T>(uri: String): Y(uri) { @Yuri companion object }
-  @Yuri open class sh<T>(uri: String): Y(uri) { @Yuri companion object }
-  @Yuri open class etc<T>(uri: String): Y(uri) { @Yuri companion object }
-  @Yuri open class vim<T>(uri: String): Y(uri) { @Yuri companion object }
-  @Yuri open class usr<T>(uri: String): Y(uri) { @Yuri companion object }
-  @Yuri open class local<T>(uri: String): Y(uri) { @Yuri companion object }
-  @Yuri companion object { @Yuri fun uri(file: Y) = File("$file") }
+  @Yuri open class bin<T>(uri: String): Y(uri) { @Yuri companion object: Y("bin") }
+  @Yuri open class sh<T>(uri: String): Y(uri) { @Yuri companion object: Y("sh") }
+  @Yuri open class etc<T>(uri: String): Y(uri) { @Yuri companion object: Y("etc") }
+  @Yuri open class vim<T>(uri: String): Y(uri) { @Yuri companion object: Y("vim") }
+  @Yuri open class usr<T>(uri: String): Y(uri) { @Yuri companion object: Y("sh") }
+  @Yuri open class local<T>(uri: String): Y(uri) { @Yuri companion object: Y("local") }
+  @Yuri companion object {
+    private val allPaths = listOf<String>(
+        "/bin",
+        "/bin/sh",
+        "/etc",
+        "/etc/vim",
+        "/usr",
+        "/usr/bin",
+        "/usr/bin/vim",
+        "/usr/local",
+        "/usr/local/bin",
+        "/usr/local/bin/sh"
+    )
+
+    @Yuri fun uri(file: Y) = File("$file")
+    @Yuri fun uris(y: Y) = allPaths.filter { it.endsWith("$y") }.map { File(it) }
+  }
+
+  fun filename() = javaClass.simpleName
+  fun original() = javaClass.simpleName
 
   override fun toString() = "$uri/${javaClass.simpleName}"
 }
 
 /**
- * localhost/
- * ├── bin/
+ * localhost
+ * ├── bin
  * │   └── sh
- * ├── etc/
+ * ├── etc
  * │   └── vim
- * └── usr/
- *     ├── bin/
+ * └── usr
+ *     ├── bin
  *     │   └── vim
- *     └── local/
- *         └── bin/
+ *     └── local
+ *         └── bin
+ *             └── sh
  */
 
-@Yuri operator fun <S: bin<localhost>> S.div(@Suppress(unused) a:sh.Companion) = sh<S>("$this")
-@Yuri operator fun <S: bin<usr<localhost>>> S.div(@Suppress(unused) a:vim.Companion) = vim<S>("$this")
-@Yuri operator fun <S: etc<localhost>> S.div(@Suppress(unused) a:vim.Companion) = vim<S>("$this")
-@Yuri operator fun <S: usr<localhost>> S.div(@Suppress(unused) a:bin.Companion) = bin<S>("$this")
-@Yuri operator fun <S: usr<localhost>> S.div(@Suppress(unused) a:local.Companion) = local<S>("$this")
-@Yuri operator fun <S: local<usr<localhost>>> S.div(@Suppress(unused) a:bin.Companion) = bin<S>("$this")
+@JvmName("00") operator fun <S: bin<localhost>> S.div(@Suppress(unused) a:sh.Companion) = sh<S>("$this")
+@JvmName("01") operator fun <S: bin<usr<localhost>>> S.div(@Suppress(unused) a:vim.Companion) = vim<S>("$this")
+@JvmName("02") operator fun <S: etc<localhost>> S.div(@Suppress(unused) a:vim.Companion) = vim<S>("$this")
+@JvmName("03") operator fun <S: usr<localhost>> S.div(@Suppress(unused) a:bin.Companion) = bin<S>("$this")
+@JvmName("04") operator fun <S: usr<localhost>> S.div(@Suppress(unused) a:local.Companion) = local<S>("$this")
+@JvmName("05") operator fun <S: local<usr<localhost>>> S.div(@Suppress(unused) a:bin.Companion) = bin<S>("$this")
+@JvmName("06") operator fun <S: bin<local<usr<localhost>>>> S.div(@Suppress(unused) a:sh.Companion) = sh<S>("$this")
+
+@JvmName("07") operator fun <S: bin<Y>> S.div(@Suppress(unused) a:sh.Companion) = sh<S>("$this")
+@JvmName("08") operator fun <S: bin<Y>> S.div(@Suppress(unused) a:vim.Companion) = vim<S>("$this")
+@JvmName("09") operator fun <S: etc<Y>> S.div(@Suppress(unused) a:vim.Companion) = vim<S>("$this")
+@JvmName("10") operator fun <S: usr<Y>> S.div(@Suppress(unused) a:bin.Companion) = bin<S>("$this")
+@JvmName("11") operator fun <S: bin<usr<Y>>> S.div(@Suppress(unused) a:bin.Companion) = vim<S>("$this")
+@JvmName("12") operator fun <S: usr<Y>> S.div(@Suppress(unused) a:local.Companion) = local<S>("$this")
+@JvmName("13") operator fun <S: local<Y>> S.div(@Suppress(unused) a:bin.Companion) = bin<S>("$this")
+@JvmName("14") operator fun <S: local<usr<Y>>> S.div(@Suppress(unused) a:bin.Companion) = bin<S>("$this")
+@JvmName("15") operator fun <S: bin<local<usr<Y>>>> S.div(@Suppress(unused) a:bin.Companion) = sh<S>("$this")
