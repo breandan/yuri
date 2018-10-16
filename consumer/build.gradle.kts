5d688ffd7450bd72a4fde20b126d4decf2129ca8
breandan <bre@ndan.co>
2018-10-15 20:04:37 -0400

    start refactoring into a proper gradle plugin

---
+++ consumer/build.gradle.kts
@@ -0,0 +1,4 @@
+plugins {
+  application
+  id("yuri-plugin") version "1.0"
+}
---
+++ plugin/build.gradle.kts
@@ -0,0 +1,55 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.psi.KtModifierListOwner
+import org.jetbrains.kotlin.renderer.KeywordStringsGenerated.KEYWORDS
+
+plugins {
+  idea apply true
+  application
+  `kotlin-dsl`
+  id("com.gradle.plugin-publish") version "0.10.0"
+  `java-gradle-plugin`
+  `maven-publish`
+}
+
+group = "co.ndan"
+version = "0.1-SNAPSHOT"
+
+buildscript {
+  dependencies.classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.0-rc-116")
+  repositories.maven("https://dl.bintray.com/kotlin/kotlin-eap")
+}
+
+repositories {
+  mavenCentral()
+  maven("https://dl.bintray.com/kotlin/kotlin-eap")
+  jcenter()
+}
+
+application.mainClassName = "main/kotlin/HelloYuriKt"
+
+dependencies {
+  compile(kotlin("stdlib-jdk8"))
+  compile(kotlin("compiler-embeddable"))
+  compile("com.squareup:kotlinpoet:1.0.0-RC1")
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions.jvmTarget = "1.8"
+}
+
+// TODO: Figure out why this doesn't work
+idea.module {
+  generatedSourceDirs.add(File("src/gen"))
+  sourceDirs.add(File("src"))
+}
+
+gradlePlugin.plugins.register("yuriPlugin") {
+  id = "yuri-plugin"
+  implementationClass = "main.kotlin.plugin.Yuri"
+}
+
+publishing {
+  repositories {
+    maven(url = "build/repository")
+  }
+}
